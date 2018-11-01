5749b6818c39345428e79ed3e78cbfb8a865f9b6
Gerardo Sabetta <sabettagerardo@gmail.com>
2018-10-31 21:46:00 -0300

    removed credentials from repo

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+
+import config from './.firebaseconfig'
+
+firebase.initializeApp(config)
+
+const db = firebase.firestore()
+// export const speakersList = db.ref('speakersList') // deprecated
+// export const interpelations = db.ref('interpelations') // deprecated
+// export const speakingArray = db.ref('speaking') // deprecated
+export const rooms = db.collection('rooms')
+export const sortedRoomsByCreatedDate = db.collection('rooms').orderBy('createdAt')
+// export const speaking = {
+//   source: db.ref('speaking'),
+//   asObject: true
+// }
